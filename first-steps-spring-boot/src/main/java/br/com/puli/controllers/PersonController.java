35ad5df62e2b18d129ec1642a2d6bf80888febaf
PuliSaglio <pulisaglio@gmail.com>
2025-02-13 12:12:43 -0300

    Introdução do conceito VO ou DTO e utilização do Dozer para aplicalo

--- first-steps-spring-boot/src/main/java/br/com/puli/controllers/PersonController.java
+++ first-steps-spring-boot/src/main/java/br/com/puli/controllers/PersonController.java
@@ -1,9 +1,11 @@
 package br.com.puli.controllers;
 
+import br.com.puli.data.vo.v1.PersonVO;
 import br.com.puli.model.Person;
 import br.com.puli.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,38 +17,34 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @RequestMapping(method = RequestMethod.GET ,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> getAll() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonVO> getAll() {
         return service.findAll();
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
+    @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(
+    public PersonVO findById(
             @PathVariable(value = "id") Long id){
         return service.findById(id);
 
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-    consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person){
+    public PersonVO create(@RequestBody PersonVO person){
         return service.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
-    consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person){
+    public PersonVO update(@RequestBody PersonVO person){
         return service.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-    method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- first-steps-spring-boot/src/main/java/br/com/puli/services/PersonServices.java
+++ first-steps-spring-boot/src/main/java/br/com/puli/services/PersonServices.java
@@ -1,44 +1,50 @@
 package br.com.puli.services;
 
+import br.com.puli.data.vo.v1.PersonVO;
 import br.com.puli.exceptions.ResourceNotFoundException;
+import br.com.puli.mapper.DozerMapper;
 import br.com.puli.model.Person;
 import br.com.puli.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
-    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
-        logger.info("Finding all Persons...");
-        return repository.findAll();
+    public List<PersonVO> findAll() {
+        logger.info("Finding all PersonVOs...");
+        return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
     }
 
 
-    public Person findById(Long id) {
-        logger.info("Finding one Person...");
+    public PersonVO findById(Long id) {
+        logger.info("Finding one PersonVO...");
 
-        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not found for this id: " + id));
+        var entity = repository.findById(id)
+                .orElseThrow(
+                ()-> new ResourceNotFoundException("Person not found for this id: " + id)
+        );
+        return DozerMapper.parseObject(entity, PersonVO.class);
     }
 
-    public Person create(Person person) {
-        logger.info("Creating new Person...");
-        return repository.save(person);
+    public PersonVO create(PersonVO person) {
+        logger.info("Creating new PersonVO...");
+        var entity = DozerMapper.parseObject(person, Person.class);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
     }
 
-    public Person update(Person person) {
-        logger.info("Updating Person...");
-        Person entity = repository.findById(person.getId())
+    public PersonVO update(PersonVO person) {
+        logger.info("Updating PersonVO...");
+        var entity = repository.findById(person.getId())
                 .orElseThrow(
                         () -> new ResourceNotFoundException("Person not found for this id: " + person.getId())
                 );
@@ -48,15 +54,16 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(person);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
     }
 
     public void delete(Long id) {
-        logger.info("Deleting Person...");
+        logger.info("Deleting PersonVO...");
 
-        Person entity = repository.findById(id)
+        var entity = repository.findById(id)
                 .orElseThrow(
-                        () -> new ResourceNotFoundException("Person not found for this id: " + id)
+                        () -> new ResourceNotFoundException("PersonVO not found for this id: " + id)
                 );
 
         repository.delete(entity);
