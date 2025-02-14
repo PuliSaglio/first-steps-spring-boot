286ab529fa4af7e0e09e01a821068a4f5aca0eed
PuliSaglio <pulisaglio@gmail.com>
2025-02-14 09:48:49 -0300

    Adicionando migrations com Flyway

--- first-steps-spring-boot/src/main/java/br/com/puli/controllers/PersonController.java
+++ first-steps-spring-boot/src/main/java/br/com/puli/controllers/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/api/person/v1")
 public class PersonController {
 
     @Autowired
---
+++ first-steps-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name` )VALUES
+                         (1,'Rua da granada','Ignacio','Male','Rossini'),
+                         (2,'rua teste','Puli','Male','Saglio'),
+                         (4,'Kentucky - USA','Muhammad','Male','Ali'),
+                         (6,'Naosei - USA','Rocky','Male','Balboa'),
+                         (9,'Mvezo - SouthAfrica','Nelson','Male','Mandela'),
+                         (10,'Smiljan - Croacia','Nikola','Male','Tesla');
+
